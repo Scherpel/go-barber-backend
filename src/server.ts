ff9803df45141c424133c94918bba1ca533029e9
Lucas Scherpel <lucasguarnelli@hotmail.com>
2021-02-16 16:27:57 -0300

    creating connection to database

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import routes from './routes';
+import './database'
+
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+
 app.listen(3333, ()=>{
-  console.log('Ok its working');
+  console.log('Server ta on bb!');
 });
