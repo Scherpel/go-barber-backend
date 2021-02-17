51c58e400cfef0f013bef0f0741f5c596566ef6b
Lucas Scherpel <lucasguarnelli@hotmail.com>
2021-02-17 15:17:11 -0300

    refactoring model file to add Entity concept

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,23 +1,21 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 interface AppointmentConstructor {
   provider: string,
   date: Date,
 }
 
 class Appointment {
+  
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({provider, date}: Omit<Appointment, 'id'>){
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-
-  }
 }
 
 export default Appointment;
