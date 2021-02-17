9ad7ff433f6421e7cf92a02cec8399912a60b855
Lucas Scherpel <lucasguarnelli@hotmail.com>
2021-02-17 18:23:49 -0300

    Updating appointment migration file

--- src/database/migrations/1613504557075-CreateAppointments.ts
+++ src/database/migrations/1613504557075-CreateAppointments.ts
@@ -11,7 +11,8 @@ export default class CreateAppointments1613504557075 implements MigrationInterfa
                         name: 'id',
                         type: 'varchar',
                         isPrimary: true,
-                        generationStrategy: 'uuid'
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
